6ac44b414a7b2e5865e816f0f55cd097718952e6
mshahwaiz-ali <241361889+mshahwaiz-ali@users.noreply.github.com>
2026-07-09 16:28:20 +0500

    Make customer profile image cleanup patch safe

--- omc_app/patches/remove_customer_profile_image_field.py
+++ omc_app/patches/remove_customer_profile_image_field.py
@@ -5,17 +5,20 @@ def execute():
     if not frappe.db.exists("DocType", "OMC Customer Profile"):
         return
 
-    meta = frappe.get_meta("OMC Customer Profile")
-    if not meta.has_field("profile_image"):
-        return
-
-    frappe.delete_doc(
+    for field_name in frappe.get_all(
         "DocField",
-        {
+        filters={
             "parent": "OMC Customer Profile",
             "fieldname": "profile_image",
         },
-        ignore_permissions=True,
-        force=True,
-    )
+        pluck="name",
+    ):
+        frappe.delete_doc(
+            "DocField",
+            field_name,
+            ignore_permissions=True,
+            force=True,
+        )
+
+    frappe.db.commit()
     frappe.clear_cache(doctype="OMC Customer Profile")
