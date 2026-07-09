0c7be1773a8d6f6c43e4d56d65ffc1bc4b2b68d0
mshahwaiz-ali <241361889+mshahwaiz-ali@users.noreply.github.com>
2026-07-09 20:34:01 +0500

    Auto-fill service document metadata

--- omc_app/omc_app/doctype/omc_service_document/omc_service_document.py
+++ omc_app/omc_app/doctype/omc_service_document/omc_service_document.py
@@ -2,6 +2,17 @@ import frappe
 from frappe.model.document import Document
 
 
+def _get_service_customer_profile(service_request):
+    if not service_request:
+        return ""
+
+    return frappe.db.get_value(
+        "OMC Service Request",
+        service_request,
+        "customer_profile",
+    ) or ""
+
+
 class OMCServiceDocument(Document):
     def before_insert(self):
         if not self.uploaded_by:
@@ -9,3 +20,24 @@ class OMCServiceDocument(Document):
 
         if not self.uploaded_on:
             self.uploaded_on = frappe.utils.now_datetime()
+
+        self._fill_customer_profile()
+        self._fill_defaults()
+
+    def before_save(self):
+        self._fill_customer_profile()
+        self._fill_defaults()
+
+    def _fill_customer_profile(self):
+        if self.service_request and not self.customer_profile:
+            self.customer_profile = _get_service_customer_profile(self.service_request)
+
+    def _fill_defaults(self):
+        if not self.source:
+            self.source = "Service Upload"
+
+        if not self.archive_reason and not self.is_archived:
+            self.archive_reason = ""
+
+        if self.is_archived and not self.archived_on:
+            self.archived_on = frappe.utils.now_datetime()
