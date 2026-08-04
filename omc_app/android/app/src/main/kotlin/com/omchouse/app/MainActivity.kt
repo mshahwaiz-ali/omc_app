6153d8149682b75b89d0e8b5b1d29ec4c5732c0a
mshahwaiz-ali <241361889+mshahwaiz-ali@users.noreply.github.com>
2026-08-04 19:45:38 +0500

    Protect sensitive app screens from system previews

--- omc_app/android/app/src/main/kotlin/com/omchouse/app/MainActivity.kt
+++ omc_app/android/app/src/main/kotlin/com/omchouse/app/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.omchouse.app
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterFragmentActivity()
+class MainActivity : FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+    }
+}
